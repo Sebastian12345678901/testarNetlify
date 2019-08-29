715206d5776d2374aa8ebc96f7065804807669ee
eriksson <sebbe.eriksson09@icloud.com>
2019-08-29 09:11:10 +0200

    kollar om det är rätt

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,7 @@ export default () => (
     <h1>Hi! I'm building a fake Gatsby site as part of a tutorial!</h1>
     <p>
       What do I like to do? Lots of course but definitely enjoy building
-      websites.
+      websites. its alive
     </p>
   </Layout>
 )
